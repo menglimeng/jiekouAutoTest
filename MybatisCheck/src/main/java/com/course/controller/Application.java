e7203b7630e01db533f4c5e678f8f93d5c2c88d0
menglimeng <1142804404@qq.com>
2020-09-10 10:29:52 +0800

    项目整合

---
+++ MybatisCheck/src/main/java/com/course/controller/Application.java
@@ -0,0 +1,18 @@
+package com.course.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+    private static ConfigurableApplicationContext context;
+    public static void main(String args){
+        Application.context = SpringApplication.run(Application.class,args);
+    }
+    public void close(){
+        Application.context.close();
+    }
+}
